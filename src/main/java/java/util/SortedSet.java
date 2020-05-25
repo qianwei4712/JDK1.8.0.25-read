f154ae9716ad17204d732e38bd981cff2a2eab0c
路的尽头在哪 <qianwei4712@163.com>
2020-05-25 21:42:28 +0800

    SortedSet.java 接口源码

--- src/main/java/java/util/SortedSet.java
+++ src/main/java/java/util/SortedSet.java
@@ -1,222 +1,75 @@
-/*
- * Copyright (c) 1998, 2013, Oracle and/or its affiliates. All rights reserved.
- * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- */
-
 package java.util;
 
 /**
- * A {@link Set} that further provides a <i>total ordering</i> on its elements.
- * The elements are ordered using their {@linkplain Comparable natural
- * ordering}, or by a {@link Comparator} typically provided at sorted
- * set creation time.  The set's iterator will traverse the set in
- * ascending element order. Several additional operations are provided
- * to take advantage of the ordering.  (This interface is the set
- * analogue of {@link SortedMap}.)
+ * 进一步提供了元素上的排序。<br/>
+ * 元素使用其自然排序进行比较，或通过通常在已排序集合创建时提供的{@link Comparator}进行排序。<br/>
+ * 集合的迭代器将以升序元素顺序遍历集合。<p>
  *
- * <p>All elements inserted into a sorted set must implement the <tt>Comparable</tt>
- * interface (or be accepted by the specified comparator).  Furthermore, all
- * such elements must be <i>mutually comparable</i>: <tt>e1.compareTo(e2)</tt>
- * (or <tt>comparator.compare(e1, e2)</tt>) must not throw a
- * <tt>ClassCastException</tt> for any elements <tt>e1</tt> and <tt>e2</tt> in
- * the sorted set.  Attempts to violate this restriction will cause the
- * offending method or constructor invocation to throw a
- * <tt>ClassCastException</tt>.
+ * 插入排序集中的所有元素必须实现<tt> Comparable </tt> 接口（或被指定的比较器）。<br/>
+ * 此外，所有这样的元素必须是<i>相互可比的</i>： <tt> e1.compareTo（e2）</tt>（或<tt> comparator.compare（e1，e2）</tt>）<br/>
+ * 若元素无法比较<tt> e1 </tt>和<tt> e2 </tt>抛出<tt> ClassCastException </tt>。<p>
  *
- * <p>Note that the ordering maintained by a sorted set (whether or not an
- * explicit comparator is provided) must be <i>consistent with equals</i> if
- * the sorted set is to correctly implement the <tt>Set</tt> interface.  (See
- * the <tt>Comparable</tt> interface or <tt>Comparator</tt> interface for a
- * precise definition of <i>consistent with equals</i>.)  This is so because
- * the <tt>Set</tt> interface is defined in terms of the <tt>equals</tt>
- * operation, but a sorted set performs all element comparisons using its
- * <tt>compareTo</tt> (or <tt>compare</tt>) method, so two elements that are
- * deemed equal by this method are, from the standpoint of the sorted set,
- * equal.  The behavior of a sorted set <i>is</i> well-defined even if its
- * ordering is inconsistent with equals; it just fails to obey the general
- * contract of the <tt>Set</tt> interface.
+ * 所有通用排序集实现类都应*提供四个“标准”构造函数：<br/>
+ * 1）一个void（无参数）构造函数，该构造函数将创建一个空排序集，并根据其元素的自然顺序进行排序。<br/>
+ * 2）具有类型为<tt> Comparator </tt>的单个参数的构造函数，它将创建一个空的排序集，并根据指定的比较器进行排序。<br/>
+ * 3）具有单个参数类型为<tt> Collection </tt>的构造函数，创建一个新的排序集合，该集合具有与其参数相同的元素，并根据元素的自然顺序进行排序。<br/>
+ * 4）具有单个类型为<tt> SortedSet </tt>的参数的构造函数，创建一个新的排序集，其元素与输入排序集的顺序相同。由于接口不能包含构造函数，因此无法强制执行此建议。<p>
  *
- * <p>All general-purpose sorted set implementation classes should
- * provide four "standard" constructors: 1) A void (no arguments)
- * constructor, which creates an empty sorted set sorted according to
- * the natural ordering of its elements.  2) A constructor with a
- * single argument of type <tt>Comparator</tt>, which creates an empty
- * sorted set sorted according to the specified comparator.  3) A
- * constructor with a single argument of type <tt>Collection</tt>,
- * which creates a new sorted set with the same elements as its
- * argument, sorted according to the natural ordering of the elements.
- * 4) A constructor with a single argument of type <tt>SortedSet</tt>,
- * which creates a new sorted set with the same elements and the same
- * ordering as the input sorted set.  There is no way to enforce this
- * recommendation, as interfaces cannot contain constructors.
- *
- * <p>Note: several methods return subsets with restricted ranges.
- * Such ranges are <i>half-open</i>, that is, they include their low
- * endpoint but not their high endpoint (where applicable).
- * If you need a <i>closed range</i> (which includes both endpoints), and
- * the element type allows for calculation of the successor of a given
- * value, merely request the subrange from <tt>lowEndpoint</tt> to
- * <tt>successor(highEndpoint)</tt>.  For example, suppose that <tt>s</tt>
- * is a sorted set of strings.  The following idiom obtains a view
- * containing all of the strings in <tt>s</tt> from <tt>low</tt> to
- * <tt>high</tt>, inclusive:<pre>
- *   SortedSet&lt;String&gt; sub = s.subSet(low, high+"\0");</pre>
- *
- * A similar technique can be used to generate an <i>open range</i> (which
- * contains neither endpoint).  The following idiom obtains a view
- * containing all of the Strings in <tt>s</tt> from <tt>low</tt> to
- * <tt>high</tt>, exclusive:<pre>
- *   SortedSet&lt;String&gt; sub = s.subSet(low+"\0", high);</pre>
- *
- * <p>This interface is a member of the
- * <a href="{@docRoot}/../technotes/guides/collections/index.html">
- * Java Collections Framework</a>.
- *
- * @param <E> the type of elements maintained by this set
- *
- * @author  Josh Bloch
- * @see Set
- * @see TreeSet
- * @see SortedMap
- * @see Collection
- * @see Comparable
- * @see Comparator
- * @see ClassCastException
+ * @param <E> 此集合所维护的元素类型
  * @since 1.2
  */
 
 public interface SortedSet<E> extends Set<E> {
     /**
-     * Returns the comparator used to order the elements in this set,
-     * or <tt>null</tt> if this set uses the {@linkplain Comparable
-     * natural ordering} of its elements.
-     *
-     * @return the comparator used to order the elements in this set,
-     *         or <tt>null</tt> if this set uses the natural ordering
-     *         of its elements
+     * @return 返回本SortedSet的比较器，如果使用自然排序则返回 null
      */
     Comparator<? super E> comparator();
 
     /**
-     * Returns a view of the portion of this set whose elements range
-     * from <tt>fromElement</tt>, inclusive, to <tt>toElement</tt>,
-     * exclusive.  (If <tt>fromElement</tt> and <tt>toElement</tt> are
-     * equal, the returned set is empty.)  The returned set is backed
-     * by this set, so changes in the returned set are reflected in
-     * this set, and vice-versa.  The returned set supports all
-     * optional set operations that this set supports.
-     *
-     * <p>The returned set will throw an <tt>IllegalArgumentException</tt>
-     * on an attempt to insert an element outside its range.
-     *
-     * @param fromElement low endpoint (inclusive) of the returned set
-     * @param toElement high endpoint (exclusive) of the returned set
-     * @return a view of the portion of this set whose elements range from
-     *         <tt>fromElement</tt>, inclusive, to <tt>toElement</tt>, exclusive
-     * @throws ClassCastException if <tt>fromElement</tt> and
-     *         <tt>toElement</tt> cannot be compared to one another using this
-     *         set's comparator (or, if the set has no comparator, using
-     *         natural ordering).  Implementations may, but are not required
-     *         to, throw this exception if <tt>fromElement</tt> or
-     *         <tt>toElement</tt> cannot be compared to elements currently in
-     *         the set.
-     * @throws NullPointerException if <tt>fromElement</tt> or
-     *         <tt>toElement</tt> is null and this set does not permit null
-     *         elements
-     * @throws IllegalArgumentException if <tt>fromElement</tt> is
-     *         greater than <tt>toElement</tt>; or if this set itself
-     *         has a restricted range, and <tt>fromElement</tt> or
-     *         <tt>toElement</tt> lies outside the bounds of the range
+     * 返回此集合部分的视图，其元素范围从<tt> fromElement </tt>（包括）到<tt> toElement </tt>（不包括）。
+     * （如果<tt> fromElement </tt>和<tt> toElement </tt>相等，则返回的集合为空。）
+     * 此集合支持返回的集合，因此返回的集合中的更改将反映在中这组，反之亦然。返回的集合支持该集合支持的所有可选集合操作。
+     * <p>超出范围抛出异常
+     * @param fromElement 当前 set中起始节点
+     * @param toElement 当前 set中高位结束节点
+     * @return 此集合一部分的视图，其元素范围从 <tt> fromElement </tt>（包括）到 <tt> toElement </tt>（不包括）
+     * @throws ClassCastException 如果使用该集合的比较器无法将<tt> fromElement </tt>和 <tt> toElement </tt>相互比较（或者，如果该集合没有比较器，则使用自然排序）。
+     *         如果无法将<tt> fromElement </tt>或 <tt> toElement </tt>与当前集中的元素进行比较，则实现可能会（但并非必须）引发此异常。
+     * @throws NullPointerException 如果起始节点或者结束节点为空，并且本 set不允许null节点
+     * @throws IllegalArgumentException 如果<tt> fromElement </tt>是大于<tt> toElement </tt>；
+     *         或如果此集合本身的范围受限制，并且<tt> fromElement </tt>或 <tt> toElement </tt>位于范围的边界之外
      */
     SortedSet<E> subSet(E fromElement, E toElement);
 
     /**
-     * Returns a view of the portion of this set whose elements are
-     * strictly less than <tt>toElement</tt>.  The returned set is
-     * backed by this set, so changes in the returned set are
-     * reflected in this set, and vice-versa.  The returned set
-     * supports all optional set operations that this set supports.
-     *
-     * <p>The returned set will throw an <tt>IllegalArgumentException</tt>
-     * on an attempt to insert an element outside its range.
-     *
-     * @param toElement high endpoint (exclusive) of the returned set
-     * @return a view of the portion of this set whose elements are strictly
-     *         less than <tt>toElement</tt>
-     * @throws ClassCastException if <tt>toElement</tt> is not compatible
-     *         with this set's comparator (or, if the set has no comparator,
-     *         if <tt>toElement</tt> does not implement {@link Comparable}).
-     *         Implementations may, but are not required to, throw this
-     *         exception if <tt>toElement</tt> cannot be compared to elements
-     *         currently in the set.
-     * @throws NullPointerException if <tt>toElement</tt> is null and
-     *         this set does not permit null elements
-     * @throws IllegalArgumentException if this set itself has a
-     *         restricted range, and <tt>toElement</tt> lies outside the
-     *         bounds of the range
+     * @param toElement e之前的元素，不包括e
+     * @return 此集合的一部分的视图，其元素严格小于<tt> toElement </tt>
+     * @throws ClassCastException 如果<tt> toElement </tt>与该集合的比较器不兼容（或者，如果集合中没有比较器，则如果<tt> toElement </ tt>不实现{@link Comparable}）。
+     *         如果不能将<tt> toElement </tt>与当前集中的元素进行比较，则实现可以（但并非必须）抛出此异常。
+     * @throws NullPointerException 如果<tt> toElement </tt>为null，并且此集合不允许null元素
+     * @throws IllegalArgumentException 如果此集合本身具有限制范围，并且<tt> toElement </tt>位于该范围的范围之外
      */
     SortedSet<E> headSet(E toElement);
 
     /**
-     * Returns a view of the portion of this set whose elements are
-     * greater than or equal to <tt>fromElement</tt>.  The returned
-     * set is backed by this set, so changes in the returned set are
-     * reflected in this set, and vice-versa.  The returned set
-     * supports all optional set operations that this set supports.
-     *
-     * <p>The returned set will throw an <tt>IllegalArgumentException</tt>
-     * on an attempt to insert an element outside its range.
-     *
-     * @param fromElement low endpoint (inclusive) of the returned set
-     * @return a view of the portion of this set whose elements are greater
-     *         than or equal to <tt>fromElement</tt>
-     * @throws ClassCastException if <tt>fromElement</tt> is not compatible
-     *         with this set's comparator (or, if the set has no comparator,
-     *         if <tt>fromElement</tt> does not implement {@link Comparable}).
-     *         Implementations may, but are not required to, throw this
-     *         exception if <tt>fromElement</tt> cannot be compared to elements
-     *         currently in the set.
-     * @throws NullPointerException if <tt>fromElement</tt> is null
-     *         and this set does not permit null elements
-     * @throws IllegalArgumentException if this set itself has a
-     *         restricted range, and <tt>fromElement</tt> lies outside the
-     *         bounds of the range
+     * @param fromElement 返回集合的指定节点 E之后的所有结点（包括E）
+     * @return 此集合中元素大于或等于<tt> fromElement </tt>的部分的视图
+     * @throws ClassCastException 如果<tt> fromElement </tt>与该集合的比较器不兼容（或者，如果集合中没有比较器，则如果<tt> fromElement </tt>不实现{@link Comparable}）。
+     *         如果<tt> fromElement </tt>无法与集合中当前的元素进行比较，则实现可能会（但并非必须）抛出此异常。
+     * @throws NullPointerException 如果<tt> fromElement </tt>为null，并且此集合不允许 null元素
+     * @throws IllegalArgumentException 如果此集合本身具有限制范围，并且<tt> fromElement </tt>位于该范围的范围之外
      */
     SortedSet<E> tailSet(E fromElement);
 
     /**
-     * Returns the first (lowest) element currently in this set.
-     *
-     * @return the first (lowest) element currently in this set
-     * @throws NoSuchElementException if this set is empty
+     * @return 返回当前 set中第一个（最低）节点
+     * @throws NoSuchElementException 如果此集合为空
      */
     E first();
 
     /**
-     * Returns the last (highest) element currently in this set.
-     *
-     * @return the last (highest) element currently in this set
-     * @throws NoSuchElementException if this set is empty
+     * @return 返回当前 set中最后（最高）一个节点
+     * @throws NoSuchElementException 如果此集合为空
      */
     E last();
 
@@ -244,11 +97,7 @@ public interface SortedSet<E> extends Set<E> {
      * The created {@code Spliterator} additionally reports
      * {@link Spliterator#SIZED}.
      *
-     * @implNote
-     * The created {@code Spliterator} additionally reports
-     * {@link Spliterator#SUBSIZED}.
-     *
-     * @return a {@code Spliterator} over the elements in this sorted set
+     * @return 在此排序集中的元素上使用{@code Spliterator}
      * @since 1.8
      */
     @Override
