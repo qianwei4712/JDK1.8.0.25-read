b2bef95d92addf217b522c4013d815a6da9cba7c
路的尽头在哪 <qianwei4712@163.com>
2020-07-18 21:16:03 +0800

    Runnable.java,Callable.java 接口源码

--- src/main/java/java/util/concurrent/Callable.java
+++ src/main/java/java/util/concurrent/Callable.java
@@ -1,64 +1,17 @@
-/*
- * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- */
-
-/*
- *
- *
- *
- *
- *
- * Written by Doug Lea with assistance from members of JCP JSR-166
- * Expert Group and released to the public domain, as explained at
- * http://creativecommons.org/publicdomain/zero/1.0/
- */
-
 package java.util.concurrent;
 
 /**
- * A task that returns a result and may throw an exception.
- * Implementors define a single method with no arguments called
- * {@code call}.
- *
- * <p>The {@code Callable} interface is similar to {@link
- * java.lang.Runnable}, in that both are designed for classes whose
- * instances are potentially executed by another thread.  A
- * {@code Runnable}, however, does not return a result and cannot
- * throw a checked exception.
- *
- * <p>The {@link Executors} class contains utility methods to
- * convert from other common forms to {@code Callable} classes.
- *
- * @see Executor
+ * 返回结果并可能引发异常的任务。 实现者定义一个没有参数的单一方法，称为 call 。
+ * Callable 接口类似于 Runnable ，因为它们都是为其实例可能由另一个线程执行的类设计的。
+ * 然而，A Runnable 不返回结果，也不能抛出被检查的异常。
+ * 该 Executors 类包含的实用方法，从其他普通形式转换为 Callable 类。
  * @since 1.5
- * @author Doug Lea
  * @param <V> the result type of method {@code call}
  */
 @FunctionalInterface
 public interface Callable<V> {
     /**
-     * Computes a result, or throws an exception if unable to do so.
-     *
+     * 计算一个返回值，如果不能计算那么抛出异常
      * @return computed result
      * @throws Exception if unable to compute a result
      */
