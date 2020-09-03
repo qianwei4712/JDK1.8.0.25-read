cde0681fec6588a47af1047f0b075c047d4baef0
路的尽头在哪 <qianwei4712@163.com>
2020-09-03 22:27:46 +0800

    WeakReference.java 弱引用

--- src/main/java/java/lang/ref/WeakReference.java
+++ src/main/java/java/lang/ref/WeakReference.java
@@ -1,69 +1,32 @@
-/*
- * Copyright (c) 1997, 2003, Oracle and/or its affiliates. All rights reserved.
- * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- */
-
 package java.lang.ref;
 
 
 /**
- * Weak reference objects, which do not prevent their referents from being
- * made finalizable, finalized, and then reclaimed.  Weak references are most
- * often used to implement canonicalizing mappings.
- *
- * <p> Suppose that the garbage collector determines at a certain point in time
- * that an object is <a href="package-summary.html#reachability">weakly
- * reachable</a>.  At that time it will atomically clear all weak references to
- * that object and all weak references to any other weakly-reachable objects
- * from which that object is reachable through a chain of strong and soft
- * references.  At the same time it will declare all of the formerly
- * weakly-reachable objects to be finalizable.  At the same time or at some
- * later time it will enqueue those newly-cleared weak references that are
- * registered with reference queues.
+ * 弱参考对象，不会阻止其指定对象的最终确定，最终确定，然后被回收。 弱引用最常用于实现规范化映射。
+ * 假设垃圾收集器在某个时间点确定对象是weakly reachable 。
+ * 那时，它将原子地清除对该对象的所有弱引用，以及所有弱引用到任何其他弱可触及的对象，通过一连串强软引用可以从该对象到达该对象。
+ * 同时，它将声明所有以前弱可触及的对象都是可以确定的。 在同一时间或稍后的时间，它将排列在引用队列中注册的新清除的弱引用。
  *
  * @author   Mark Reinhold
  * @since    1.2
  */
-
 public class WeakReference<T> extends Reference<T> {
 
     /**
-     * Creates a new weak reference that refers to the given object.  The new
-     * reference is not registered with any queue.
+     * 创建引用给定对象的新的弱引用。
+     * 新引用未注册到任何队列。
      *
-     * @param referent object the new weak reference will refer to
+     * @param referent 新的弱引用将引用
      */
     public WeakReference(T referent) {
         super(referent);
     }
 
     /**
-     * Creates a new weak reference that refers to the given object and is
-     * registered with the given queue.
+     * 创建引用给定对象并在给定队列中注册的新的弱引用。
      *
-     * @param referent object the new weak reference will refer to
-     * @param q the queue with which the reference is to be registered,
-     *          or <tt>null</tt> if registration is not required
+     * @param referent 新的弱引用将引用
+     * @param q 要注册参考的队列，如果不需要注册， 则为 null
      */
     public WeakReference(T referent, ReferenceQueue<? super T> q) {
         super(referent, q);
