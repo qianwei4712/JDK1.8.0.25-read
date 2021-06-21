4ecba28005aa83a630887d3281b43808a914bc4d
路的尽头在哪 <qianwei4712@163.com>
2021-06-21 21:04:04 +0800

    RunnableFuture.java

--- src/main/java/java/util/concurrent/RunnableFuture.java
+++ src/main/java/java/util/concurrent/RunnableFuture.java
@@ -1,54 +1,17 @@
-/*
- * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- */
-
-/*
- *
- *
- *
- *
- *
- * Written by Doug Lea with assistance from members of JCP JSR-166
- * Expert Group and released to the public domain, as explained at
- * http://creativecommons.org/publicdomain/zero/1.0/
- */
-
 package java.util.concurrent;
 
 /**
- * A {@link Future} that is {@link Runnable}. Successful execution of
- * the {@code run} method causes completion of the {@code Future}
- * and allows access to its results.
+ * A {@link Future} that is {@link Runnable}.
+ * {@code run} 方法的成功执行会导致 {@code Future} 的完成并允许访问其结果。
  * @see FutureTask
  * @see Executor
  * @since 1.6
  * @author Doug Lea
- * @param <V> The result type returned by this Future's {@code get} method
+ * @param <V> 此 Future 的 {@code get} 方法返回的结果类型
  */
 public interface RunnableFuture<V> extends Runnable, Future<V> {
     /**
-     * Sets this Future to the result of its computation
-     * unless it has been cancelled.
+     * 将此 Future 设置为其计算结果，除非它已被取消。
      */
     void run();
 }
