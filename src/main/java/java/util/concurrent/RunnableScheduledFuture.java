05b7f44a538d83fd77272531958e467738795729
路的尽头在哪 <qianwei4712@163.com>
2021-06-21 21:05:06 +0800

    RunnableScheduledFuture.java

--- src/main/java/java/util/concurrent/RunnableScheduledFuture.java
+++ src/main/java/java/util/concurrent/RunnableScheduledFuture.java
@@ -1,58 +1,21 @@
-/*
- * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- */
-
-/*
- *
- *
- *
- *
- *
- * Written by Doug Lea with assistance from members of JCP JSR-166
- * Expert Group and released to the public domain, as explained at
- * http://creativecommons.org/publicdomain/zero/1.0/
- */
-
 package java.util.concurrent;
 
 /**
- * A {@link ScheduledFuture} that is {@link Runnable}. Successful
- * execution of the {@code run} method causes completion of the
- * {@code Future} and allows access to its results.
+ * A {@link ScheduledFuture} that is {@link Runnable}.
+ * {@code run} 方法的成功执行会导致 {@code Future} 的完成并允许访问其结果。
  * @see FutureTask
  * @see Executor
  * @since 1.6
  * @author Doug Lea
- * @param <V> The result type returned by this Future's {@code get} method
+ * @param <V> 此 Future 的 {@code get} 方法返回的结果类型
  */
 public interface RunnableScheduledFuture<V> extends RunnableFuture<V>, ScheduledFuture<V> {
 
     /**
-     * Returns {@code true} if this task is periodic. A periodic task may
-     * re-run according to some schedule. A non-periodic task can be
-     * run only once.
+     * 如果此任务是周期性的，则返回 {@code true}。周期性任务可能会根据某个计划重新运行。
+     * 一个非周期性任务只能运行一次。
      *
-     * @return {@code true} if this task is periodic
+     * @return {@code true} 如果此任务是周期性的
      */
     boolean isPeriodic();
 }
